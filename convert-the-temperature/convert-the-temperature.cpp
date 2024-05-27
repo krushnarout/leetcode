7317a2b964c4227bd354cd4405fb868add5954de
Krushna Kanta Rout <krushnakantarout90@gmail.com>
2024-05-27 11:27:36 +0530

    Time: 0 ms (100%), Space: 7.61 MB (39.74%)

---
+++ convert-the-temperature/convert-the-temperature.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<double> convertTemperature(double celsius) {
+        double k = celsius + 273.15;
+        double f = celsius * 1.80 + 32.00;
+
+        vector<double> arr;
+        arr.push_back(k);
+        arr.push_back(f);
+        return arr;
+    }
+};
