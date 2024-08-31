2c5d09460c3939320992f40fac6ff2ceb83dab7e
Krushna Kanta Rout <129386740+krushnarout@users.noreply.github.com>
2024-08-31 20:03:42 +0530

    Time: 72 ms (33.44%), Space: 45.18 MB (45.61%)

---
+++ next-greater-node-in-linked-list/next-greater-node-in-linked-list.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> nextLargerNodes(ListNode* head) {
+        vector<int>arr;
+        ListNode*temp=head;
+        int n=0;
+        while(temp){
+            arr.push_back(temp->val);
+            temp=temp->next;
+            n++;
+        }
+        vector<int>ans(n);
+        stack<int>st;
+        ans[n-1]=0;
+        st.push(arr[n-1]);
+        for(int i=n-2; i>=0; i--){
+            while(st.size()>0 && st.top()<=arr[i]){
+                st.pop();
+            }
+            if(st.size()==0) ans[i]=0;
+            else ans[i]=st.top();
+            st.push(arr[i]);
+        }
+        return ans;
+    }
+};
